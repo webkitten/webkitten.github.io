a399fab0209deca89efb491e7b56edafb3d3690e
webkitten <webkitten@list.ru>
2016-11-19 00:20:56 +0700

    and one more

--- aj-json-test/main.js
+++ aj-json-test/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 	console.log("Hi!")
-	var data1 = JSON.parse($("#data1").html());
-	console.log(data1)
+	$.ajax({
+		url: "https://cdn.rawgit.com/webkitten/jsonp-test/master/2009g-06m.txt",
+		dataType: 'text',
+		crossDomain: true,
+		success: function(results){
+			console.log(JSON.parse(results))
+		},
+		error: function(err) {
+			console.log(err)
+		}
+	});
+
 })
