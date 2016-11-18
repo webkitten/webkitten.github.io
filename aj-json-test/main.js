007046b7750df791e5125244617a05c934238e13
webkitten <webkitten@list.ru>
2016-11-18 23:59:12 +0700

    ajax query add

--- aj-json-test/main.js
+++ aj-json-test/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 	console.log("Hi!")
+	$.ajax({
+		url: "https://github.com/webkitten/jsonp-test/blob/master/2009g-06m.json",
+		dataType: 'jsonp',
+		crossDomain: true,
+		success: function(results){
+			console.log(results)
+		},
+		error: function(err) {
+			console.log(err)
+		};
+	});
+
 })
