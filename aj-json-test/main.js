a98d179196edc5ae19c3961340b9f962638a26b3
webkitten <webkitten@list.ru>
2016-11-19 00:30:24 +0700

    ok zhe

--- aj-json-test/main.js
+++ aj-json-test/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	console.log("Hi!")
 	$.ajax({
-		url: "https://cdn.rawgit.com/webkitten/jsonp-test/master/2009g-06m.txt",
+		url: "https://cdn.rawgit.com/webkitten/jsonp-test/master/2010g-01m.txt",
 		dataType: 'text',
 		crossDomain: true,
 		success: function(results){
