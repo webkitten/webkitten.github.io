291f746fa8c65da2643a5a44e1591b3193588047
webkitten <webkitten.dev@gmail.com>
2017-08-31 09:14:15 +0700

    fastpush ___ 9:14:15,12

--- js/all.js
+++ js/all.js
@@ -1020,12 +1020,12 @@ $(document).ready(function() {
 	$(".section_header").animated("fadeInUp", "fadeOutDown");
 
 	//$(".animation_1").animated("flipInY", "rotateOut");
-	$(".animation_1").animated("fadeInUp", "fadeOutDown");
-	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
-	$(".animation_3").animated("fadeInRight", "fadeOutDown");
+	// $(".animation_1").animated("fadeInUp", "fadeOutDown");
+	// $(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	// $(".animation_3").animated("fadeInRight", "fadeOutDown");
 
-	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
-	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+	// $(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	// $(".right .resume_item").animated("fadeInRight", "fadeOutDown");
 
 	function heightDetect() {
 		$(".main_head").css("height", $(window).height());
